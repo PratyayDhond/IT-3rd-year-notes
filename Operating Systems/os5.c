dcb91186ad7ed84bf45a7d414654124565251298
Pratyay Dhond <dhondpratyay@gmail.com>
2021-11-24 21:07:05 +0530

    OS practical codes

---
+++ Operating Systems/os5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main(){
+	int executionTime[10];
+	int arrivalTime[10];
+	int n;
+	int 
+	return 0;
+}
+
+/*
+printf("\nPname\tarrivaltime\texecutiontime\twaitingtime\ttatime");
+for(i=0; i<n; i++)
+printf("\n%s\t%5d\t\t%5d\t\t%5d\t\t%5d",pn[i],at[i],et[i],wt[i],ta[i]);
+printf("\nAverage waiting time is:%f",awt);
+*/
