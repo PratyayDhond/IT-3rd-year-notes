5aad6fa7fbc47d3a9f95889df7cc00149df3a4d9
PratyayDhond <dhondpratyay@gmail.com>
2022-04-18 16:50:28 +0530

    Linux practical 7 output

---
+++ 6th Sem/Linux/7/choices.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+  int age;  
+ printf("Enter your age: ");
+ scanf("%d",&age);
+
+  if(age>18){
+     printf("Drink waterrrrr\n");
+   }else{
+     printf("Drink Milk\n");
+    }   
+}
